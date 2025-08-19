ff52f87ab3949e7a69129c8e7719860216a1cb4c
chandan <87321332+chandanvura@users.noreply.github.com>
2025-08-19 15:49:26 +0530

    Add files via upload
    New Upload

---
+++ simplewebapp/src/main/java/com/telusko/simplewebapp/SimplewebappApplication.java
@@ -0,0 +1,14 @@
+package com.telusko.simplewebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class SimplewebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplewebappApplication.class, args);
+	}
+
+}
